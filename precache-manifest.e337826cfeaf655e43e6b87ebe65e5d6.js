715fb2ca7bd6aff5e56b10b7cab84496d64cadb8
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-01-01 15:58:56 +0300

    Updates

---
+++ precache-manifest.e337826cfeaf655e43e6b87ebe65e5d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3b49ef298d83ae1b994553b94c44a5",
+    "url": "/Crwn-Clothing/index.html"
+  },
+  {
+    "revision": "ee3ca7c272a36bda6b20",
+    "url": "/Crwn-Clothing/static/css/main.c53472b6.chunk.css"
+  },
+  {
+    "revision": "92946774df9a2278ae0e",
+    "url": "/Crwn-Clothing/static/js/2.4132db81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Crwn-Clothing/static/js/2.4132db81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee3ca7c272a36bda6b20",
+    "url": "/Crwn-Clothing/static/js/main.36b2e396.chunk.js"
+  },
+  {
+    "revision": "256c66e006e3a85ba45c",
+    "url": "/Crwn-Clothing/static/js/runtime-main.f71e7432.js"
+  }
+]);
