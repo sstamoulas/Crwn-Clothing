ea9b184d7d43284813ac838daefa24a83de0e22f
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-01-03 18:13:39 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Crwn-Clothing/precache-manifest.2bf9281d76ff422c828b9d4479e31f3d.js"
+  "/Crwn-Clothing/precache-manifest.8b0f9a4f45e7f70ee1a29922a06056a6.js"
 );
 
 self.addEventListener('message', (event) => {
