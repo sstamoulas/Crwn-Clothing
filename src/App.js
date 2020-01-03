dfdf89430f4f35aa2b3844fff3d52cd7d2e19875
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-01-03 22:13:54 +0300

    Added Sign-In-And-Sign-Up component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
@@ -20,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/sign-in' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
