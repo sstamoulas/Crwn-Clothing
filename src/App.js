c92b521e8dd07072efd280c52858ea8c03cab446
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-01-03 18:32:32 +0300

    Moved images to public folder, added header component and links to home, shop-page and contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
 
@@ -15,6 +16,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
