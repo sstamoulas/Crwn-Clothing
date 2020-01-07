9cf7d3fa45d784c811f010d4d4ff76beec683dff
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-01-07 12:21:20 +0300

    Added redux and implemented userReducer and userActions to Header and App components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
 import Header from './components/header/header.component';
+
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
 
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           }, 
           () => {
             console.log(this.state)
@@ -39,7 +35,7 @@ class App extends Component {
         });
       }
       else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -51,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
@@ -62,4 +58,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
-  <BrowserRouter basename={'Crwn-Clothing'}>
-    <App />
-  </BrowserRouter>, 
+  <Provider store={store}>
+    <BrowserRouter basename={'Crwn-Clothing'}>
+      <App />
+    </BrowserRouter>
+  </Provider>, 
   document.getElementById('root')
 );
