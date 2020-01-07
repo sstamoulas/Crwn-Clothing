ca7b10d6d8119970fa61631b837f0df3604026b5
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-01-07 12:22:44 +0300

    Updates

---
+++ precache-manifest.aa66e9487ac49799dab9bb01e7bab372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118156f024da5db4815af85921305b17",
+    "url": "/Crwn-Clothing/index.html"
+  },
+  {
+    "revision": "78271fa7775ca385802f",
+    "url": "/Crwn-Clothing/static/css/main.ca6ae413.chunk.css"
+  },
+  {
+    "revision": "a018221b16403704bbe9",
+    "url": "/Crwn-Clothing/static/js/2.a9af3ffa.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/Crwn-Clothing/static/js/2.a9af3ffa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78271fa7775ca385802f",
+    "url": "/Crwn-Clothing/static/js/main.011c0e2b.chunk.js"
+  },
+  {
+    "revision": "256c66e006e3a85ba45c",
+    "url": "/Crwn-Clothing/static/js/runtime-main.f71e7432.js"
+  },
+  {
+    "revision": "04fd000d2f3a940764b05409a05ba552",
+    "url": "/Crwn-Clothing/static/media/crown.04fd000d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Crwn-Clothing/precache-manifest.83df1675870a27595196955089c512e6.js"
+  "/Crwn-Clothing/precache-manifest.aa66e9487ac49799dab9bb01e7bab372.js"
 );
 
 self.addEventListener('message', (event) => {
