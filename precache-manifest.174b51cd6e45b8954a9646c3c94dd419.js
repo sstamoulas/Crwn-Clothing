0ad8cabe91ee957e39077f637b52c0b2fecf0e3d
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-02-01 15:04:22 +0300

    Updates

---
+++ precache-manifest.174b51cd6e45b8954a9646c3c94dd419.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c0803842f0576eb50a0391afc78e52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b3b5d26f1e76a17f589",
+    "url": "/static/css/main.47991481.chunk.css"
+  },
+  {
+    "revision": "fedb8722349e459cd84c",
+    "url": "/static/js/2.55fed101.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.55fed101.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b3b5d26f1e76a17f589",
+    "url": "/static/js/main.1e2a83e8.chunk.js"
+  },
+  {
+    "revision": "00b0b827894e3cf48415",
+    "url": "/static/js/runtime-main.a5ad0cbb.js"
+  },
+  {
+    "revision": "04fd000d2f3a940764b05409a05ba552",
+    "url": "/static/media/crown.04fd000d.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
