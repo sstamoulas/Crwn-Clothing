db58c7222f94634d677765c7b1785ad9fcdfc809
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-08-23 16:44:54 +0300

    separating our CRA app into client folder, added express folder to help handle backend payment

---
+++ client/build/precache-manifest.562da8556319f918e9c963f76da1f86d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d09ba2fdfa402ecbf5bfb51909ca3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f287cbb7717f804a69d",
+    "url": "/static/css/main.5bbe26d3.chunk.css"
+  },
+  {
+    "revision": "2fe1d147c0da2d23a4b5",
+    "url": "/static/js/2.e9f27d7c.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.e9f27d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f287cbb7717f804a69d",
+    "url": "/static/js/main.f9414a8c.chunk.js"
+  },
+  {
+    "revision": "a4d0ed5da9a8b38f8c34",
+    "url": "/static/js/runtime-main.43f589d4.js"
+  },
+  {
+    "revision": "04fd000d2f3a940764b05409a05ba552",
+    "url": "/static/media/crown.04fd000d.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
